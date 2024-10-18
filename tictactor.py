a3f9d502ff800e5ed825eb1487081fcc754169c8
Ashish Kasturia <ashoooo@hotmail.com>
2024-10-18 15:30:54 -0400

    files

---
+++ tictactor.py
@@ -0,0 +1,91 @@
+from random import random
+
+def tictactor():
+    board = ["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]
+
+    name1 = input("Player 1 name?")
+    name2 = input("Player 2 name")
+    win = False
+    while win == False:
+        print("", board[0][0], "|", board[0][1], "|", board[0][2])
+        print("---|---|---")
+        print("", board[1][0], "|", board[1][1], "|", board[1][2])
+        print("---|---|---")
+        print("", board[2][0], "|", board[2][1], "|", board[2][2])
+        print("===========================================================")
+        
+        available = False
+        print("Turn:", name1)
+        while available == False:
+            row = input("What row would you like to place an X in? (1-3)")
+            while isnum(row) == False or int(row) > 3 or int(row) < 1:
+                print("Please enter a number in range")
+                row = input("What row would you like to place an X in? (1-3)")
+            column = input("What column would you like to place an X in? (1-3)")
+            while isnum(column) == False or int(column) > 3 or int(column) < 1:
+                print("Please enter a number in range")
+                column = input("What column would you like to place an X in? (1-3)")
+            if board[int(row) - 1][int(column) - 1] != "-":
+                print("That space is not available!")
+            else:
+                board[int(row) - 1][int(column) - 1] = "X"
+                available = True
+        
+        for i in range(3):
+            if board[i][0] == "X" and board[i][1] == "X" and board[i][2] == "X":
+                return name1, "has won!"
+            if board[0][i] == "X" and board[1][i] == "X" and board[2][i] == "X":
+                return name1, "has won!"
+        if board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+            return name1, "has won!"
+        if board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+            return name1, "has won!"
+        
+        print("", board[0][0], "|", board[0][1], "|", board[0][2])
+        print("---|---|---")
+        print("", board[1][0], "|", board[1][1], "|", board[1][2])
+        print("---|---|---")
+        print("", board[2][0], "|", board[2][1], "|", board[2][2])
+        print("===========================================================")
+
+        available = False
+        print("Turn:", name2)
+        while available == False:
+            row = input("What row would you like to place an O in? (1-3)")
+            while isnum(row) == False or int(row) > 3 or int(row) < 1:
+                print("Please enter a number in range")
+                row = input("What row would you like to place an O in? (1-3)")
+            column = input("What column would you like to place an O in? (1-3)")
+            while isnum(column) == False or int(column) > 3 or int(column) < 1:
+                print("Please enter a number in range")
+                column = input("What column would you like to place an O in? (1-3)")
+            if board[int(row) - 1][int(column) - 1] != "-":
+                print("That space is not available!")
+            else:
+                board[int(row) - 1][int(column) - 1] = "O"
+                available = True
+        
+        for i in range(3):
+            if board[i][0] == "O" and board[i][1] == "O" and board[i][2] == "O":
+                return name2, "has won!"
+            if board[0][i] == "O" and board[1][i] == "O" and board[2][i] == "O":
+                return name2, "has won!"
+        if board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+            return name2, "has won!"
+        if board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+            return name2, "has won!"
+        
+
+# ----------------------------------------------------------------------------------------------------------
+
+
+def isnum(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+
+if __name__ == "__main__":
+    tictactor()
