22f2a3b858418811d8f4d42979bf4e5ee801e2c4
Ashish Kasturia <ashoooo@hotmail.com>
2024-10-18 15:53:09 -0400

    added draw

--- tictactor.py
+++ tictactor.py
@@ -6,6 +6,7 @@ def tictactor():
     name1 = input("Player 1 name?")
     name2 = input("Player 2 name")
     win = False
+    numOfTurns = 0
     while win == False:
         print("", board[0][0], "|", board[0][1], "|", board[0][2])
         print("---|---|---")
@@ -33,14 +34,30 @@ def tictactor():
         
         for i in range(3):
             if board[i][0] == "X" and board[i][1] == "X" and board[i][2] == "X":
+                print(name1, "has won!")
                 return name1, "has won!"
             if board[0][i] == "X" and board[1][i] == "X" and board[2][i] == "X":
+                print(name1, "has won!")
                 return name1, "has won!"
         if board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+            print(name1, "has won!")
             return name1, "has won!"
         if board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+            print(name1, "has won!")
             return name1, "has won!"
         
+        numOfTurns += 1
+        
+        if numOfTurns == 9:
+            print("", board[0][0], "|", board[0][1], "|", board[0][2])
+            print("---|---|---")
+            print("", board[1][0], "|", board[1][1], "|", board[1][2])
+            print("---|---|---")
+            print("", board[2][0], "|", board[2][1], "|", board[2][2])
+            print("===========================================================")
+            print("Draw")
+            break
+
         print("", board[0][0], "|", board[0][1], "|", board[0][2])
         print("---|---|---")
         print("", board[1][0], "|", board[1][1], "|", board[1][2])
@@ -64,17 +81,23 @@ def tictactor():
             else:
                 board[int(row) - 1][int(column) - 1] = "O"
                 available = True
-        
+
         for i in range(3):
             if board[i][0] == "O" and board[i][1] == "O" and board[i][2] == "O":
+                print(name2, "has won!")
                 return name2, "has won!"
             if board[0][i] == "O" and board[1][i] == "O" and board[2][i] == "O":
+                print(name2, "has won!")
                 return name2, "has won!"
         if board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+            print(name2, "has won!")
             return name2, "has won!"
         if board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+            print(name2, "has won!")
             return name2, "has won!"
         
+        numOfTurns += 1
+    return "Draw"
 
 # ----------------------------------------------------------------------------------------------------------
 
